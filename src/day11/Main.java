faed968cd2e3709cf7c86e936dd9d01c4df30357
Mathias <mathias.steger@uni-oldenburg.de>
2017-12-11 13:13:55 +0100

    Update

--- src/day11/Main.java
+++ src/day11/Main.java
@@ -1,81 +1,72 @@
 package day11;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import util.ReadInputHelper;
 
 public class Main {
-public static void main(String[] args) {
-    ArrayList<String> lines = new ReadInputHelper(11).getLines();
-    String line = lines.get(0);
-    City city = new City();
-    int rslt = 0;
-    String[] ins = line.split(",");
+	public static void main(String[] args) {
+		ArrayList<String> lines = new ReadInputHelper(11).getLines();
 
-    for (int i = 0; i < ins.length; i++) {
-        try {
-            if (i > 0) {
-                city.getClass().getMethod(ins[i], String.class).invoke(city, ins[i - 1]);
-            } else {
-                city.getClass().getMethod(ins[i], String.class).invoke(city, "");
-            }
-        } catch (Exception e){
-        }
+		String line = lines.get(0);
+		// String line = "ne,s";
+		City city = new City();
+		int rslt = 0;
+		String[] ins = line.split(",");
 
-        System.out.println(city.x + ", " + city.y + ", max: " + city.max());
-        if (city.max() > rslt)
-            rslt = city.max();
-    }
+		for (int i = 0; i < ins.length; i++) {
 
-    System.out.println(city.x + ", " + city.y);
-    System.out.println(city.max());
-    System.out.println(rslt);
-}
+			try {
+				city.getClass().getMethod(ins[i]).invoke(city);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+
+			if (city.max() > rslt)
+				rslt = city.max();
+		}
+
+		System.out.println(city.x + ", " + city.y + ", " + city.z);
+		System.out.println(city.max());
+		System.out.println(rslt);
+	}
 }
 
 class City {
-public int x, y = 0;
+	public int x = 0, y = 0, z = 0;
 
-public void n(String last) {
-    this.y--;
-}
+	synchronized public void n() {
+		y++;
+		z--;
+	}
 
-public void ne(String last) {
-    if (!last.equals("nw")) {
-        this.y--;
-    }
-    this.x++;
-}
+	synchronized public void ne() {
+		x--;
+		y++;
+	}
 
-public void se(String last) {
-    if (!last.equals("sw"))
-        y++;
-    this.x++;
-}
+	synchronized public void se() {
+		x--;
+		z++;
+	}
 
-public void s(String last) {
-    y++;
-}
+	synchronized public void s() {
+		y--;
+		z++;
+	}
 
-public void sw(String last) {
-    if (!last.equals("se"))
-        y++;
-    x--;
-}
+	synchronized public void sw() {
+		x++;
+		y--;
+	}
 
-public void nw(String last) {
-    if (!last.equals("ne"))
-        y--;
-    x--;
-}
+	synchronized public void nw() {
+		z--;
+		x++;
+	}
 
-public int max() {
-    if ((x < 0 && y < 0) || (x > 0 && y > 0)) {
-        return Math.abs(x + y);
-    } else if (Math.abs(x) > Math.abs(y)) {
-        return Math.abs(x);
-    } else {
-        return Math.abs(y);
-    }
-}
+	synchronized public int max() {
+		return (Math.abs(x) + Math.abs(y) + Math.abs(z))/2;
+	}
 }
