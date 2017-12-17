5da156b6ff4e80b1564a4d00328e340e909fd839
hira3th <mathiassteger@web.de>
2017-12-17 07:46:28 +0100

    Day 17 update

--- src/day17/Main.java
+++ src/day17/Main.java
@@ -1,11 +1,9 @@
 package day17;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.apache.commons.lang3.ArrayUtils;
 
-import util.ReadInputHelper;
 
 public class Main {
 	public static void main(String[] args) {
@@ -18,11 +16,6 @@ public class Main {
 			pos = ((pos + input) % list.length)+1;
 			
 			list = ArrayUtils.insert(pos, list, i);
-			
-			if(i % 1000000 == 0)
-				System.out.println(i);
-			if (i < 5)
-				System.out.println(Arrays.toString(list));
 		}
 
 		System.out.println(Arrays.toString(list));
