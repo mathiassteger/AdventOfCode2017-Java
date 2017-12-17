eafae5c9babe9ac6af720be08c7b9d3bc093a737
hira3th <mathiassteger@web.de>
2017-12-17 07:56:05 +0100

    Day 17 update

--- src/day17/Main.java
+++ src/day17/Main.java
@@ -1,25 +1,16 @@
 package day17;
 
-import java.util.Arrays;
-
-import org.apache.commons.lang3.ArrayUtils;
-
-
 public class Main {
 	public static void main(String[] args) {
 		int input = 377;
 		int pos = 0;
-		int[] list = {0};
-
 		
-		for (int i = 1; i < 5000000; i++) {
-			pos = ((pos + input) % list.length)+1;
-			
-			list = ArrayUtils.insert(pos, list, i);
+		for (int i = 1; i < 50000001; i++) {
+			pos = ((pos + input) % i) + 1;
+
+			if (pos == 1) {
+				System.out.println(i);
+			}
 		}
-
-		System.out.println(Arrays.toString(list));
-
-		System.out.println(list[ArrayUtils.indexOf(list, 0)+1]);
 	}
 }
